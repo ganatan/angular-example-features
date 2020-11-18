67df95e51b992de049f2b841c91e635a3b197198
ganatan <dannyganatan@gmail.com>
2020-11-18 11:34:19 +0100

    Update Angular CLI 11.0.1

--- example-api-rest/src/app/example-api-rest/21-continents/exercice.component.spec.ts
+++ example-api-rest/src/app/example-api-rest/21-continents/exercice.component.spec.ts
@@ -1,15 +1,17 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { ExerciceComponent } from './exercice.component';
 import { HttpClientModule } from '@angular/common/http';
+import { RouterTestingModule } from '@angular/router/testing';
 
-describe('get-all - Component', () => {
+describe('Continents - Component', () => {
   let component: ExerciceComponent;
   let fixture: ComponentFixture<ExerciceComponent>;
 
   beforeEach(async () => {
     TestBed.configureTestingModule({
-      imports: [HttpClientModule],
+      imports: [
+        HttpClientModule,
+        RouterTestingModule],
       declarations: [ExerciceComponent]
     })
       .compileComponents();
